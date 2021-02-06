4723c26b9cfce1642b6473715f9696780e9a8596
Tim Holman <timothy.w.holman@gmail.com>
2021-02-06 18:32:26 -0500

    small cleanups

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-(() => {
-  const canvas = document.querySelector("#canvas");
-  const context = canvas.getContext("2d");
+;(() => {
+  const canvas = document.querySelector("#canvas")
+  const context = canvas.getContext("2d")
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
 
-  context.lineWidth = 10;
-  const rectangles = [];
-  let splitDirectionVertical = true;
+  context.lineWidth = 10
+  const rectangles = []
+  let splitDirectionVertical = true
 
-  canvas.addEventListener("click", onRectangleClick);
-
-  function createRectangle(x, y, width, height, color) {
-    rectangles.push({ x, y, width, height, color });
-  }
+  canvas.addEventListener("click", onRectangleClick)
 
   function drawRectangles() {
     rectangles.forEach((rectangle) => {
-      context.fillStyle = rectangle.color;
-      context.beginPath();
-      context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-      context.closePath();
-      context.fill();
-      context.stroke();
-    });
+      context.fillStyle = rectangle.color
+      context.beginPath()
+      context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height)
+      context.closePath()
+      context.fill()
+      context.stroke()
+    })
   }
 
   function onRectangleClick(e) {
-    // TODO: Find the actual rectangle that was clicked
     const clickedIndex = rectangles.findIndex((rectangle) => {
       if (
         e.x > rectangle.x &&
@@ -35,23 +30,23 @@
         e.y > rectangle.y &&
         e.y < rectangle.y + rectangle.height
       ) {
-        return true;
+        return true
       }
-    });
+    })
 
     if (clickedIndex === -1) {
-      splitDirectionVertical = !splitDirectionVertical;
-      return;
+      splitDirectionVertical = !splitDirectionVertical
+      return
     }
 
-    const clickedRectangle = rectangles[clickedIndex];
+    const clickedRectangle = rectangles[clickedIndex]
 
-    rectangles.splice(clickedIndex, 1);
+    rectangles.splice(clickedIndex, 1)
 
     splitRectangleAt(clickedRectangle, {
       x: e.x - clickedRectangle.x,
       y: e.y - clickedRectangle.y,
-    });
+    })
   }
 
   function splitRectangleAt(rectangle, position) {
@@ -62,14 +57,14 @@
         width: position.x,
         height: rectangle.height,
         color: getColor(),
-      });
+      })
       rectangles.push({
         x: rectangle.x + position.x,
         y: rectangle.y,
         width: rectangle.width - position.x,
         height: rectangle.height,
         color: getColor(),
-      });
+      })
     } else {
       rectangles.push({
         x: rectangle.x,
@@ -77,18 +72,18 @@
         width: rectangle.width,
         height: position.y,
         color: getColor(),
-      });
+      })
       rectangles.push({
         x: rectangle.x,
         y: rectangle.y + position.y,
         width: rectangle.width,
         height: rectangle.height - position.y,
         color: getColor(),
-      });
+      })
     }
 
-    splitDirectionVertical = !splitDirectionVertical;
-    drawRectangles();
+    splitDirectionVertical = !splitDirectionVertical
+    drawRectangles()
   }
 
   function getColor() {
@@ -101,13 +96,24 @@
       "#EBEBED",
       "#EBEBED",
       "#EBEBED",
+      "#EBEBED",
+      "#EBEBED",
+      "#EBEBED",
+      "#EBEBED",
+      "#EBEBED",
       "#C53632",
       "#3E4984",
       "#F8DD67",
-    ];
-    return colors[Math.floor(Math.random() * colors.length)];
+    ]
+    return colors[Math.floor(Math.random() * colors.length)]
   }
 
-  createRectangle(0, 0, window.innerWidth, window.innerHeight, "#EBEBED");
-  drawRectangles();
-})();
+  rectangles.push({
+    x: 0,
+    y: 0,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    color: "#EBEBED",
+  })
+  drawRectangles()
+})()
